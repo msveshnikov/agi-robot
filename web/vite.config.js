9e714839745bda42d8dcfb8d7455413c98b997b0
Max Sveshnikov <msveshnikov@gmail.com>
2026-02-07 20:46:55 +0100

    feat: Add Vite configuration for React development with API and Socket.IO proxy.

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+            '/socket.io': {
+                target: 'http://localhost:3000',
+                ws: true,
+            },
+        },
+    },
 })
