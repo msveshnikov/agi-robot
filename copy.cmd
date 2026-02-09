4030db04a2e847fc00d21fae4a5e7c56790bb35e
Max Sveshnikov <msveshnikov@gmail.com>
2026-02-09 14:47:38 +0100

    1

---
+++ copy.cmd
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "REMOTE_USER=ubuntu"
+set "REMOTE_HOST=mvpgen.com"
+set "REMOTE_DIR=/home/ubuntu"
+set "DB_NAME=agi-robot"
+set "CONTAINER_NAME=agi-robot-mongodb"
+
+echo Starting export of MongoDB collections...
+
+ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection blogposts --type=csv --fields=_id,title,content,author,createdAt,updatedAt,slug,isPublished,tags,likes,views > blogposts.csv"
+ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection cognitivelogs --type=csv --fields=_id,plan,subplan,memory,goal,timestamp,createdAt,updatedAt > cognitivelogs.csv"
+ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection commandlogs --type=csv --fields=_id,timestamp,command_type,command_data,llm_response,source,createdAt,updatedAt > commandlogs.csv"
+ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection robotstates --type=csv --fields=_id,agi,asi,speed,panic,forward,back,left,right,lang,goal,rgb.hue,rgb.sat,rgb.bri,rgb.swi,arm1,arm2,distance,temperature,humidity,plan,subplan,space_map,movement_history,memory,alarm,updated_at,createdAt,updatedAt > robotstates.csv"
+ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection telemetrylogs --type=csv --fields=_id,timestamp,distance,temperature,humidity,position_estimate.x,position_estimate.y,createdAt,updatedAt > telemetrylogs.csv"
+
+echo Downloading collections...
+scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/blogposts.csv .
+scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/cognitivelogs.csv .
+scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/commandlogs.csv .
+scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/robotstates.csv .
+scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/telemetrylogs.csv .
+
+echo Done.
