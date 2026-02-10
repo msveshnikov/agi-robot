c9634179e2c7da65f3ec0ac8590d86f36b21ca00
Max Sveshnikov <msveshnikov@gmail.com>
2026-02-10 18:00:31 +0100

    Minor

--- web/src/pages/Dashboard.jsx
+++ web/src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 import {
     Gauge,
--- web/src/pages/LandingPage.jsx
+++ web/src/pages/LandingPage.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-// eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import {
