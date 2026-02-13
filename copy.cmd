614f5e01a87d388322527864e419f9ccaed9cd5a
Max Sveshnikov <msveshnikov@gmail.com>
2026-02-13 21:03:20 +0100

    WIP

--- copy.cmd
+++ copy.cmd
@@ -14,6 +14,7 @@ ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --d
 ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection commandlogs --type=csv --fields=_id,timestamp,command_type,command_data,llm_response,source > commandlogs.csv"
 ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection robotstates --type=csv --fields=_id,agi,asi,speed,panic,forward,back,left,right,lang,goal,rgb.hue,rgb.sat,rgb.bri,rgb.swi,arm1,arm2,distance,temperature,humidity,plan,subplan,space_map,movement_history,memory,alarm,timestamp > robotstates.csv"
 ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection telemetrylogs --type=csv --fields=_id,timestamp,distance,temperature,humidity,position_estimate.x,position_estimate.y > telemetrylogs.csv"
+ssh -l %REMOTE_USER% %REMOTE_HOST% "docker exec %CONTAINER_NAME% mongoexport --db %DB_NAME% --collection waitlists --type=csv --fields=_id,email,plan,timestamp > waitlists.csv"
 
 echo Downloading collections...
 scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/blogposts.csv .
@@ -21,5 +22,6 @@ scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/cognitivelogs.csv .
 scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/commandlogs.csv .
 scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/robotstates.csv .
 scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/telemetrylogs.csv .
+scp %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_DIR%/waitlists.csv .
 
 echo Done.
