03481120129f8022755b6885fc5f85bc9680452a
Max Sveshnikov <msveshnikov@gmail.com>
2026-02-25 21:40:30 +0100

    Update server.js

--- web/backend/server.js
+++ web/backend/server.js
@@ -100,7 +100,7 @@ httpServer.listen(PORT, () => {
 });
 
 // Daily Robot Blog Post Generation
-cron.schedule("0 10 * * *", async () => {
+cron.schedule("0 21 * * *", async () => {
     console.log("🤖 Running daily blog post generation check...");
 
     try {
